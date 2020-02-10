bdbce4b1149d42e985321efc0b061e39d7d36b6c
Swastika Library <rahul1.shc@gmail.com>
2020-02-10 22:10:20 +0530

    added routes

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-const http = require('http');
+//const http = require('http');
 const express=require('express');
 
 const app = express();
 //between creating app and creatig server we can add middlewares
-app.use((req, res, next)=>{
+/*app.use((req, res, next)=>{
     console.log('In the middleware');
     next();
+})*/
+
+
+app.use('/users', (req, res, next)=>{
+    //won't work unless we call next in above app.use
+    //console.log('In the 2nd middleware');
+    res.send('<h1>Users</h1>');
 })
 
-
-app.use((req, res, next)=>{
+app.use('/', (req, res, next)=>{
     //won't work unless we call next in above app.use
-    console.log('In the 2nd middleware');
+    //console.log('In the 2nd middleware');
+    res.send('<h1>Others</h1>');
 })
 
 //between creating app and creatig server we can add middlewares
-const server = http.createServer(app);
+/*const server = http.createServer(app);
 
-server.listen(3000);
+server.listen(3000);*/
+
+app.listen(3000);
