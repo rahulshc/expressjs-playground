83fd959edafae42210f99dba578d7ccd51d7a7e4
Swastika Library <rahul1.shc@gmail.com>
2020-02-22 00:01:13 +0530

    added routes and served static files

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 //const http = require('http');
 const express=require('express');
-
+const bodyParser=require('body-parser');
 const app = express();
+const adminRoutes=require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 //between creating app and creatig server we can add middlewares
-/*app.use((req, res, next)=>{
-    console.log('In the middleware');
+
+
+/*app.use('/', (req, res, next)=>{
+    console.log('This always runs');
     next();
-})*/
+});*/
 
+//bodyparser.urlencoded internally calls the next
+app.use(bodyParser.urlencoded({extended: false}));
+//order of admin routes and shop routes matters here if we used app.use in admin and shop routes
+app.use('/admin',adminRoutes);//appendes all admin routes with/admin
+app.use(shopRoutes);
 
-app.use('/users', (req, res, next)=>{
-    //won't work unless we call next in above app.use
-    //console.log('In the 2nd middleware');
-    res.send('<h1>Users</h1>');
-})
+//handling all unknown routes because route next funnels from top to bottom
 
-app.use('/', (req, res, next)=>{
-    //won't work unless we call next in above app.use
-    //console.log('In the 2nd middleware');
-    res.send('<h1>Others</h1>');
-})
+app.use((req, res, next)=> {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
 
 //between creating app and creatig server we can add middlewares
 /*const server = http.createServer(app);
