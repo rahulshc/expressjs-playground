d60ab13c1a957507f208e25cb60d75ae852c7352
Swastika Library <rahul1.shc@gmail.com>
2020-02-22 11:49:58 +0530

    added a rootdir implementation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //const http = require('http');
 const express=require('express');
 const bodyParser=require('body-parser');
+const path=require('path');
 const app = express();
 const adminRoutes=require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -21,7 +22,8 @@ app.use(shopRoutes);
 //handling all unknown routes because route next funnels from top to bottom
 
 app.use((req, res, next)=> {
-    res.status(404).send('<h1>Page not found</h1>');
+    //res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, './', 'views', '404.html'));
 });
 
 
