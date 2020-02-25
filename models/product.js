ee284200e18d6a2ed5dfccbe40d697e8dcb59b26
Swastika Library <rahul1.shc@gmail.com>
2020-02-25 21:44:30 +0530

    added a helper function

--- models/product.js
+++ models/product.js
@@ -3,6 +3,20 @@ const path=require('path');
 const p =path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
 //const products = [];
 
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if(err) 
+        {
+            console.log('1'+':'+err);
+             cb([]);
+        }
+        else{
+            cb(JSON.parse(fileContent));
+        }
+        
+    });
+}
+
 module.exports = class Product {
     constructor(title){
         this.title=title;
@@ -10,14 +24,10 @@ module.exports = class Product {
 
     save(){
         //products.push(this);
-        fs.readFile(p, (err, fileContent)=> {
-            let products = [];
-            if(!err){
-                products = JSON.parse(fileContent);//reads a json file and convert this to js object
-            }
+        getProductsFromFile(products=>{
             products.push(this);//this will work because of arrow function it refers to the class
             fs.writeFile(p, JSON.stringify(products), (err)=> {
-                console.log(err);
+                console.log('2'+':'+err);
             });//converts to json from js object
         });
     }
@@ -36,14 +46,7 @@ module.exports = class Product {
     }*/
 
     static fetchAll(cb) {
-        fs.readFile(p, (err, fileContent) => {
-            if(err) 
-            {
-                console.log(err);
-                return cb([]);
-            }
-            return cb(JSON.parse(fileContent));
-        });
+        getProductsFromFile(cb);
        
     }
 }
